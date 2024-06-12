29744cb941ab9c578350d4be701beacecbc72199
Mustafa Çınar <108414767+mustafacnar@users.noreply.github.com>
2024-06-13 01:29:10 +0300

    Update models.py

--- products/models.py
+++ products/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class SalesChannel(models.Model):
+    sales_channel_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.sales_channel_name
+
+
 class Product(models.Model):
     product_name = models.CharField(max_length=255, unique=True)
     product_quantity = models.PositiveIntegerField()
@@ -17,17 +24,7 @@ class Bundle(models.Model):
     name = models.CharField(max_length=255)
     products = models.ManyToManyField(Product)
     stock = models.PositiveIntegerField()
+    sales_channel = models.ForeignKey(SalesChannel, on_delete=models.CASCADE, blank=True)
 
     def __str__(self):
         return self.name
-
-
-class SalesChannel(models.Model):
-    sales_channel_name = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.sales_channel_name
-
-    def save(self, *args, **kwargs):
-        self.sales_channel_name = self.sales_channel_name.title()
-        super().save(*args, **kwargs)
